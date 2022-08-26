2f40c6ac02be7c9f2531bd630421ec9fc394924e
MasterJ <masterj122517@163.com>
2022-08-26 17:25:11 +0800

    changed some dwm-status

--- dwm-status-refresh.sh
+++ dwm-status-refresh.sh
@@ -157,8 +157,8 @@ get_bytes
 vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
 vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
 
-#xsetroot -name "🐻🥶👹 $(print_mem)M ⬇️ $vel_recv ⬆️ $vel_trans $(dwm_alsa) $(print_bat) $(show_record)⌛ $(print_date) "
-xsetroot -name "🐻🥶👹 $(cpu) $(dwm_disk) $(dwm_alsa) $(print_bat) $(show_record)⌛ $(print_date) "
+xsetroot -name "$(print_mem)M ⬇️ $vel_recv ⬆️ $vel_trans $(dwm_alsa) $(print_bat) $(show_record)⌛ $(print_date) "
+#xsetroot -name "🐻🥶👹 $(cpu) $(dwm_disk) $(dwm_alsa) $(print_bat) $(show_record)⌛ $(print_date) "
 # Update old values to perform new calculations
 old_received_bytes=$received_bytes
 old_transmitted_bytes=$transmitted_bytes
