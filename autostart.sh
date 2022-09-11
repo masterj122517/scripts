adabdeb67de97838618687719894a1ef79a76a8a
masterj <masterj122517@163.com>
2022-09-11 14:15:43 +0800

    add convert doc to pdf function on compiler

--- autostart.sh
+++ autostart.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-picom --experimental-backends -b
+#picom --experimental-backends -b
+#picom &
 #/bin/bash ~/scripts/xmodmap.sh & 
-dunst &
-remaps &
+#dunst &
+#remaps &
 #notify-send "failure is not an option"
 #notify-send "You should bet on yourself"
 #notify-send "Learn to stay focus at home that is really important!!!"
+
 #/bin/bash ~/scripts/setxmodmap-colemak.sh & 
 #/bin/bash ~/scripts/xmodmap-emacs.sh & 
 #variety & 
 /bin/bash ~/scripts/dwm-status.sh & 
 #fcitx &
 
-xwallpaper --zoom ~/.local/share/bg
+#xwallpaper --zoom ~/.local/share/bg
+pipewire &
 
 libinput-gestures-setup start & 
